9737dea6a7e6e1307bfb7c339c717a77b49040fe
poco-irrilevante <charles.korthout@gmail.com>
2014-04-27 16:29:32 +0200

    changed mechanics of program, miss interpeted the question

--- run_analysis.R
+++ run_analysis.R
@@ -1,54 +1,18 @@
 ## R Getting and Cleaning Data Peer Assessment 
-install.packages("LaF") # for processing fixed width files
-library(LaF)
+source("read_data.R") #sub routine for reading from tran and test set
+source("renderColumnName.R") # for rendering column names
 ## Set the working directory. 
-setwd("~/Courses/R-GettIngAndCleaningData/Data/UCI HAR Dataset")
+setwd("~/Courses/R-GettIngAndCleaningData/Data/RunAnalysis")
+directory <- "~/Courses/R-GettIngAndCleaningData/Data/RunAnalysis/UCI HAR Dataset"
 
-## Read the column data
-## the column labels are stored in the file features.txt, separated by a blank
-## 
-cnames <- read.table("./features.txt",sep=' ', stringsAsFactors=FALSE)[,2]
-columnCount <- length(cnames)
-str(cnames)
 
-##
-## It shows that there are 561 columns
-## Read in a text line to determine the line length
-lineWidth <- nchar(readLines("./train/X_train.txt")[1])
-lineWidth
+# read the train and testdata 
+train <- read_data(directory,"train")
+test <- read_data(directory,"test")
+consolidated <- rbind(train, test)
 
-## each line has 8976 characters, calculate the column width
-columnWidth <- lineWidth / columnCount
-columnWidth
+# fix column namings 
+colnames(consolidated) <- renderColumnName(colnames(consolidated))
 
-## read in the data file using the "laf_open_fwf" tool
-## it looks like each line has 561 columns, each containing 16 characters
-## we build vectors for columntypes and column widths using rep & column count
-file <- laf_open_fwf(filename = "./train/X_train.txt",
-                      column_types=rep("double", columnCount),
-                      column_names=cnames,
-                      column_widths=rep(columnWidth,columnCount))
-## We are only interested in the x,y,z means and the standard deviations
-## contained in column 1 to 6
-train <- file[,1:6]
-str(train)
-trainrowCount <- nrow(train) ## count the rows in the train dataset
-
-## Read in the test data, using the column width and column count 
-## calculated before
-file2 <- laf_open_fwf(filename = "./test/X_test.txt",
-                     column_types=rep("double", columnCount),
-                     column_names=cnames,
-                     column_widths=rep(columnWidth,columnCount))
-## again only the means and stds
-test <- file2[,1:6]
-str(test)
-testrowCount <- nrow(test) ## count the rows in the test dataset
-## combine the two data frames into the "tidy" data frame.
-tidy <- rbind(train,test)
-colnames(tidy) = c("mean.X", "mean.Y", "mean.Z","std.X", "std.Y", "std.Z")
-str(tidy)
-nrow(tidy)==trainrowCount+testrowCount ## do the combined rows match?
-
-## Write the tidy data frame to disk
-write.csv(tidy, "tidy.csv", row.names=FALSE)
+#Write the tidy data frame to disk
+write.csv(consolidated, "tidy.csv", row.names=FALSE)
